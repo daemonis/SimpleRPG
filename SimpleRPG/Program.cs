a3cf6f1d5990ba1f8f6a0f9d86033203b962135c
Melanie Zeller <melanie.zeller@verizon.net>
2022-02-27 17:23:43 -0800

    Added other input validation. Working on actual adventure.

--- SimpleRPG/Program.cs
+++ SimpleRPG/Program.cs
@@ -1,50 +1,94 @@
 ﻿class SimpleRPG
 {
-    private static string?[] playerChoices = new string[3];
-    private static string worldName = "Meridia";
+    private readonly static string?[] playerChoices = new string[3]; // 0: Name, 1: location, 2: pronoun choice.
+    private readonly static string[] towns = { "Steelswamp", "Ironhold", "Meadowland", "Forestview", "Groverest" };
+    private readonly static string worldName = "Meridia";
     public static void Main()
     {
         //Intro
 
-        WriteText($"Welcome, adventurer, to the world of {worldName}.\n\nPress any key to start your adventure...");
-        Console.ReadKey(true);
+        IntroToMeridia();
 
         // Collect character information.
 
+        CollectCharacterInformation();
+
+        // Adventure Start. (WIP)
+
+        Console.Clear();
+
+        WriteText($"It is a dark and stormy night in the town of {towns[0]}. You can hear the sound of the rain tapping on the roof of the inn and the gentle flow of water coursing through the surrounding wetlands.\n\nThe inn you decided to take refuge in is not the best, but it beats being out in the rain. The innkeep also gave you a great deal on the room you are staying in.\n\nYou are currently laying in bed. The blankets are made from a rough wool that isn't too comfortable, but bound to keep any adventurer warm. You have a nightstand next to you, agaist which you've leaned your sword. Your bag rests atop the nightstand.");
+
+        // Game will prompt user for input. Based on input, do a specific action. (WIP)
+    }
+    public static void IntroToMeridia()
+    {
+        WriteText($"Welcome, adventurer, to the world of {worldName}.\n\nPress any key to start your adventure...");
+        Console.ReadKey(true);
+    }
+    public static void CollectCharacterInformation()
+    {
         Console.Clear();
 
         WriteText("What be your name, traveler?");
-        bool nameIsNull = true;
 
-        playerChoices[0] = ValidateInput(playerChoices[0], nameIsNull); // Checks if null. If null then prompts to enter another value.
+        playerChoices[0] = ValidateInput(); // Checks if null. If null then prompts to enter another value.
 
         WriteText($"\nGreetings, {playerChoices[0]}. From where do you hail?");
-        bool homeIsNull = true;
 
-        playerChoices[1] = ValidateInput(playerChoices[1], homeIsNull); // Checks if null. If null then prompts for another value.
+        playerChoices[1] = ValidateInput(); // Checks if null. If null then prompts for another value.
 
-        WriteText($"\n{playerChoices[0]} of {playerChoices[1]}. That has a ring to it, doesn't it? Now, are ye a lass or lad?");
-        bool pronounChoiceIsNull = true;
+        WriteText($"\n{playerChoices[0]} of {playerChoices[1]}. That has a nice ring to it, doesn't it? Now, are ye a lass or lad?");
 
-        playerChoices[2] = ValidateInput(playerChoices[2], pronounChoiceIsNull); // Checks if null. If null then prompts for another value.
+        playerChoices[2] = ValidateInput(); // Checks if null. If null then prompts for another value.
 
         WriteText("\nAlright. Let's get this adventure started.\n\nPress any key to continue...");
         Console.ReadKey(true);
-
-        // Adventure Start
-
-        Console.Clear();
-
-        WriteText("It is a dark and stormy night in the town of Steelswamp. You can hear the sound of the rain tapping on the roof of the inn and the gentle flow of water coursing through the surrounding wetlands.\n\nThe inn you decided to take refuge in is not the best, but it beats being out in the rain. The innkeep also gave you a great deal on the room you are staying in.\n\nYou are currently laying in bed. The blankets are made from a rough wool that isn't too comfortable, but bound to keep any adventurer warm. You have a nightstand next to you, agaist which you've leaned your sword. Your bag rests atop the nightstand.");
     }
-    public static string? GetText() => Console.ReadLine(); // Get user input.
-    public static void WriteText(string input) // Used for "typing" effect on RPG prompts.
+    public static void WriteText(string input) // Used for "typing" effect on RPG prompts. Takes type string.
     {
         foreach (char ch in input)
         {
             Console.Write(ch);
             Thread.Sleep(50);
         }
+    }
+    public static string? ValidateInput() // Prompts if field is null, numeric, or not between 3-15.
+    {
+        bool inputIsNull = false;
+        bool inputIsNumeric = false;
+        bool inputIsCorrectLength = true;
+        string? inputChoice;
+
+        do
+        {
+            Console.Write("\nPlease enter an answer: ");
+
+            inputChoice = Console.ReadLine();
+            inputIsNull = IsInputNull(inputChoice);
+
+            if (inputIsNull)
+            {
+                continue;
+            }
+
+            inputIsNumeric = IsNumeric(inputChoice);
+
+            if (inputIsNumeric)
+            {
+                continue;
+            }
+
+            inputIsCorrectLength = IsInputCorrectLength(inputChoice);
+
+            if (!inputIsCorrectLength)
+            {
+                continue;
+            }
+        }
+        while (inputIsNull || inputIsNumeric || !inputIsCorrectLength);
+
+        return inputChoice;
     }
     public static bool IsInputNull(string? input) // Checks if field is null. Takes nullable type string.
     {
@@ -58,16 +102,32 @@
             return false;
         }
     }
-    public static string? ValidateInput(string? inputChoice, bool inputIsNull) // Prompts if field is null. Takes nullable type string and boolean value.
+    public static bool IsNumeric(string? inputChoice) // Checks if field is numeric. Takes nullable type string.
     {
-        do
+        foreach (char ch in inputChoice)
         {
-            Console.Write("\nPlease enter an answer: ");
-            inputChoice = GetText();
-            inputIsNull = IsInputNull(inputChoice);
-            continue;
+            if (!Char.IsLetter(ch))
+            {
+                Console.Write("Answer must not contain a number.");
+                return true;
+            }
         }
-        while (inputIsNull);
-        return inputChoice;
+
+        return false;
+    }
+    public static bool IsInputCorrectLength(string? inputChoice) // Checks if field is between 3-15. Takes nullable type string.
+    {
+        int minimumInputLength = 3;
+        int maximumInputLength = 15;
+
+        if (inputChoice.Length < minimumInputLength || inputChoice.Length > maximumInputLength)
+        {
+            Console.Write($"Answer must be between {minimumInputLength}-{maximumInputLength} characters.");
+            return false;
+        }
+        else
+        {
+            return true;
+        }
     }
 }
