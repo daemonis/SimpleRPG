703882ca7723dc9c6310da756b50c519dabda1f2
Melanie Zeller <melanie.zeller@verizon.net>
2022-03-03 00:54:52 -0800

    Stylistic changes.

--- SimpleRPG/Program.cs
+++ SimpleRPG/Program.cs
@@ -9,13 +9,15 @@
 
         IntroToMeridia();
 
+        Console.Clear();
+
         // Collect character information.
 
         CollectCharacterInformation();
 
-        // Adventure Start. (WIP)
-
         Console.Clear();
+
+        // Adventure Start. (WIP)
 
         WriteText($"It is a dark and stormy night in the town of {towns[0]}. You can hear the sound of the rain tapping on the roof of the inn and the gentle flow of water coursing through the surrounding wetlands.\n\nThe inn you decided to take refuge in is not the best, but it beats being out in the rain. The innkeep also gave you a great deal on the room you are staying in.\n\nYou are currently laying in bed. The blankets are made from a rough wool that isn't too comfortable, but bound to keep any adventurer warm. You have a nightstand next to you, agaist which you've leaned your sword. Your bag rests atop the nightstand.");
 
@@ -24,25 +26,25 @@
     public static void IntroToMeridia()
     {
         WriteText($"Welcome, adventurer, to the world of {worldName}.\n\nPress any key to start your adventure...");
+
         Console.ReadKey(true);
     }
     public static void CollectCharacterInformation()
     {
-        Console.Clear();
-
         WriteText("What be your name, traveler?");
 
-        playerChoices[0] = ValidateInput(); // Checks if null. If null then prompts to enter another value.
+        playerChoices[0] = ValidateAndGetInput(); // Checks if null. If null then prompts to enter another value.
 
         WriteText($"\nGreetings, {playerChoices[0]}. From where do you hail?");
 
-        playerChoices[1] = ValidateInput(); // Checks if null. If null then prompts for another value.
+        playerChoices[1] = ValidateAndGetInput(); // Checks if null. If null then prompts for another value.
 
         WriteText($"\n{playerChoices[0]} of {playerChoices[1]}. That has a nice ring to it, doesn't it? Now, are ye a lass or lad?");
 
-        playerChoices[2] = ValidateInput(); // Checks if null. If null then prompts for another value.
+        playerChoices[2] = ValidateAndGetInput(); // Checks if null. If null then prompts for another value.
 
         WriteText("\nAlright. Let's get this adventure started.\n\nPress any key to continue...");
+
         Console.ReadKey(true);
     }
     public static void WriteText(string input) // Used for "typing" effect on RPG prompts. Takes type string.
@@ -53,11 +55,9 @@
             Thread.Sleep(50);
         }
     }
-    public static string? ValidateInput() // Prompts if field is null, numeric, or not between 3-15.
+    public static string? ValidateAndGetInput() // Prompts if field is null, numeric, or not between 3-15.
     {
-        bool inputIsNull = false;
-        bool inputIsNumeric = false;
-        bool inputIsCorrectLength = true;
+        bool inputIsNull = false, inputIsNumeric = false, inputIsCorrectLength = true;
         string? inputChoice;
 
         do
@@ -65,6 +65,7 @@
             Console.Write("\nPlease enter an answer: ");
 
             inputChoice = Console.ReadLine();
+
             inputIsNull = IsInputNull(inputChoice);
 
             if (inputIsNull)
