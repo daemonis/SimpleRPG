c91f0c8852d39f312814e48860890bccaeaf5931
Melanie Zeller <melanie.zeller@verizon.net>
2022-03-09 16:31:46 -0800

    Added NPC and Monster logic (somewhat).

--- SimpleRPG/Monster.cs
+++ SimpleRPG/Monster.cs
@@ -4,6 +4,29 @@
     {
         public Monster()
         {
+            Name = "Ghoul";
+            Level = 1;
+            HitPoints = 10;
         }
+        public Monster(string name)
+        {
+            Name = name;
+            Level = 1;
+            HitPoints = 10;
+        }
+        public Monster(string name, int level, int hitPoints)
+        {
+            Name = name;
+            Level = level;
+            HitPoints = hitPoints;
+        }
+        private string name;
+        public string Name { get; }
+
+        private int level;
+        public int Level { get; }
+
+        private int hitPoints;
+        public int HitPoints { get; }
     }
 }
--- SimpleRPG/NPC.cs
+++ SimpleRPG/NPC.cs
@@ -4,6 +4,23 @@
     {
         public NPC()
         {
+            Name = "Adventurer";
+            Level = 1;
+            HitPoints = 25;
         }
+        public NPC(string name, int level, int hitPoints)
+        {
+            Name = name;
+            Level = level;
+            HitPoints = hitPoints;
+        }
+        private string name;
+        public string Name { get; }
+
+        private int level;
+        public int Level { get; }
+
+        private int hitPoints;
+        public int HitPoints { get; }
     }
 }
